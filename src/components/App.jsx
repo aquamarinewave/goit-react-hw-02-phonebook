628a89a4d8322a77273db7980f81c173f121ff52
Maryna Medynska <maryna.medynska@gmail.com>
2023-02-27 00:08:25 +0200

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,12 +9,7 @@ import Filter from './Filter/Filter';
 class App extends Component {
   
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   }
 
@@ -43,9 +38,11 @@ class App extends Component {
 
   render() {
 
-    const normalizedFilter = this.state.filter.toLowerCase();
+    const {filter, contacts} = this.state;
 
-    const visibleContacts = this.state.contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter))
+    const normalizedFilter = filter.toLowerCase();
+
+    const visibleContacts = contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter))
 
     return (
       <Container>
